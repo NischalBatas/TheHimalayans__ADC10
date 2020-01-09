b4be8d55c9e042f71942ccd149af07de964175b4
NischalBatas <58929522+NischalBatas@users.noreply.github.com>
2020-01-10 01:09:49 +0545

    "Second changes"

--- TheHimalayans__ADC10/settings.py
+++ TheHimalayans__ADC10/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b+a2-jrm6c==qafsp+($&50jd%g+o=m8o+p5@2m&apquk!unea'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =True
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products'
 ]
 
 MIDDLEWARE = [
--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name=models.TextField()
+    product_price=models.FloatField()
+    product_category=models.TextField()
+    product_image=models.ImageField()
